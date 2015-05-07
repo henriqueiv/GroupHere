8aa8e5a53612010c330ae776a5bb578c2c7e5937
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2015-05-07 16:17:38 -0300

    Login

---
+++ GroupHere/Controller/LoginRegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginRegisterViewController.swift
+//  GroupHere
+//
+//  Created by Henrique Valcanaia on 5/7/15.
+//  Copyright (c) 2015 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class LoginRegisterViewController: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
